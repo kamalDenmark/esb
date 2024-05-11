6afa877b8754b8c57c737977f53558768bfb5f7b
mka <mka@avc.dk>
2024-05-11 16:09:58 +0200

    dfdsdfs

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React quicly
+          Learn React quicly and softly
         </a>
       </header>
     </div>
